5063f28f2bfdbdc2d2bd54d431a8b08a9872ac5c
sinbox0701 <psh090953@gmail.com>
2021-01-28 14:22:44 +0900

    Styling Movies

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React from "react";
 import axios from "axios";
 import Movie from "./Movie";
+import "./App.css"
 
 class App extends React.Component{
   state = {
@@ -37,16 +38,19 @@ class App extends React.Component{
         <div>
           <span class="loader__text">Loading...</span>
         </div>
-        ) : ( movies.map(movie => {
-            return <Movie
-                    key={movie.id} 
-                    id={movie.id} 
-                    year={movie.year} 
-                    title={movie.title} 
-                    summary={movie.summary} 
-                    poster={movie.medium_cover_image} 
-                    />
-          })
+        ) : ( 
+          <div class="movies">
+            {movies.map(movie => (
+            <Movie
+             key={movie.id} 
+             id={movie.id} 
+             year={movie.year} 
+             title={movie.title} 
+             summary={movie.summary} 
+             poster={movie.medium_cover_image} 
+            />
+            ))}
+          </div>
         )
       }
     </section>
