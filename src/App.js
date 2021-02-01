c42fa34ece6edc1a230701ef10dc9dcd139929ba
sinbox0701 <psh090953@gmail.com>
2021-02-01 12:44:28 +0900

    Getting Ready for the Router

--- src/App.js
+++ src/App.js
@@ -1,62 +1,9 @@
 import React from "react";
-import axios from "axios";
-import Movie from "./Movie";
-import "./App.css"
 
-class App extends React.Component{
-  state = {
-    isLoading: true,
-    movies: []
-  };
-
-  getMovies = async () =>{
-    //const movies = await axios.get("https://yts-proxy.nomadcoders1.now.sh/list_movies.json");
-    //console.log(movies);
-    //console.log(movies.data.data.movies);
-    const {
-      data:{
-        data:{
-          movies
-        }
-      }
-    } = await axios.get("https://yts-proxy.nomadcoders1.now.sh/list_movies.json?sort_by=rating");
-    //console.log(movies);
-    this.setState({movies, isLoading:false});//{movies:movies}
-  }
-
-  componentDidMount(){
-    this.getMovies();
- 
-  }
-  
-  render(){
-    const {isLoading, movies} = this.state;
-    return (
-    <section className="container">
-      {
-        isLoading ? ( 
-        <div>
-          <span className="loader__text">Loading...</span>
-        </div>
-        ) : ( 
-          <div className="movies">
-            {movies.map(movie => (
-            <Movie
-             key={movie.id} 
-             id={movie.id} 
-             year={movie.year} 
-             title={movie.title} 
-             summary={movie.summary} 
-             poster={movie.medium_cover_image} 
-             genres={movie.genres}
-            />
-            ))}
-          </div>
-        )
-      }
-    </section>
-    );
-  }
+function App() {
+  return (
+    <span>something</span>
+  );
 }
 
 export default App;
